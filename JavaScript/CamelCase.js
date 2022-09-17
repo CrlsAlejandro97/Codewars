10602238a5d70dbd3a802a382cc758e8ea25194c
Alejandro Ocampo <83668936+CrlsAlejandro97@users.noreply.github.com>
2022-09-17 20:52:48 -0300

    Update CamelCase.js

--- JavaScript/CamelCase.js
+++ JavaScript/CamelCase.js
@@ -1,7 +1,7 @@
 const message = "the--stealth-warrior"
 
 function convertString (message){
- //1.todo lo que sea letra se reemplaza por " "|2.Todos los blancos por 1 blanco|3. La primer letra por mayuscula
+ //1.todo lo que no sea letra se reemplaza por " "|2.Todos los blancos por 1 blanco|3. La primer letra por mayuscula
  message = message.replace(/\W/gi, " ").replace(/\s+/," ").replace(/\b[a-z]/gi,letra => letra.toUpperCase() )
  console.log(message)
   }
